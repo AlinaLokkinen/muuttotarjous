c40b1a2fd395f753c0678feb533f41698ceb53fb
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-09-05 13:36:50 +0300

    create project

---
+++ muuttotarjous/src/main/java/alinan/projekti/muuttotarjous/MuuttotarjousApplication.java
@@ -0,0 +1,13 @@
+package alinan.projekti.muuttotarjous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuuttotarjousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuuttotarjousApplication.class, args);
+	}
+
+}
---
+++ muuttotarjous/src/test/java/alinan/projekti/muuttotarjous/MuuttotarjousApplicationTests.java
@@ -0,0 +1,13 @@
+package alinan.projekti.muuttotarjous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuuttotarjousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
